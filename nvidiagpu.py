6e7967b39e8ded493612484ca4f3f76907531ac6
Vili Lipo <vili@deb.Lipo>
2018-07-30 21:18:50 +0300

    averages and refractor

--- nvidiagpu.py
+++ nvidiagpu.py
@@ -1,30 +1,39 @@
 """High level wrapper for accessing gpu information"""
 from py3nvml.py3nvml import *
 import sys
+from math import floor
+
 
 class NvidiaGPU():
     """Accesses nvidia information through nvidia-smi"""
 
     def __init__(self):
         """Init monitoring"""
-        self.mintemp = sys.maxsize
-        self.maxtemp = sys.maxsize * -1
-        self.minclock = sys.maxsize
-        self.maxclock = sys.maxsize * -1
-        self.minfan = sys.maxsize
-        self.maxfan = sys.maxsize * -1
-        self.powerMax = sys.maxsize * -1
-        self.powerMin = sys.maxsize
-        self.gpuUtilMin = sys.maxsize
-        self.gpuUtilMax = sys.maxsize * -1
-        self.memoryUtilMax = sys.maxsize * -1
-        self.memoryUtilMin = sys.maxsize
-        self.memoryClockMax = sys.maxsize * -1
-        self.memoryClockMin = sys.maxsize
-        self.minMemUtil = sys.maxsize
-        self.maxMemUtil = sys.maxsize * -1
+        self.recorders = []
+        self.temp_recorder = ValueRecorder(self.getGpuTemp)
+        self.recorders.append(self.temp_recorder)
+        self.gpu_clock_recorder = ValueRecorder(self.getGpuClock)
+        self.recorders.append(self.gpu_clock_recorder)
+        self.memory_clock_recorder = ValueRecorder(
+                self.getGpuMemoryClock)
+        self.recorders.append(self.memory_clock_recorder)
+        self.fan_recorder = ValueRecorder(self.getGpuFanSpeed)
+        self.recorders.append(self.fan_recorder)
+        self.power_recorder = ValueRecorder(self.getGpuPowerUsage)
+        self.recorders.append(self.power_recorder)
+        self.gpu_util_recorder = ValueRecorder(self.getGpuUtilization)
+        self.recorders.append(self.gpu_util_recorder)
+        self.mem_util_recorder = ValueRecorder(self.getMemoryUtilization)
+        self.recorders.append(self.mem_util_recorder)
         nvmlInit()
         self.handle = nvmlDeviceGetHandleByIndex(0)
+
+    def update(self):
+        """Updates the values for recorders"""
+        for recorder in self.recorders:
+            recorder.update()
+        self.getPerfCapReason()
+
     def getGpuTemp(self):
         try:
             temp_value = int(str(nvmlDeviceGetTemperature(self.handle,
@@ -32,8 +41,6 @@ class NvidiaGPU():
         except NVMLError as e:
             print("Cant get temp:" + e)
             return -273
-        self.maxtemp = max(self.maxtemp, temp_value)
-        self.mintemp = min(self.mintemp, temp_value)
         return temp_value
 
     def getGpuClock(self):
@@ -43,31 +50,23 @@ class NvidiaGPU():
         except NVMLError as e:
             print("Can't get clock:" + e)
             return -1
-        self.maxclock = max(self.maxclock, gpuclock)
-        self.minclock = min(self.minclock, gpuclock)
         return gpuclock
 
     def getGpuMemoryClock(self):
         """Get current GPU clock speed in Mhz"""
-        memoryclock =int(str(nvmlDeviceGetClockInfo(self.handle, 2)))
-        self.memoryClockMax = max(self.memoryClockMax, memoryclock)
-        self.memoryClockMin = min(self.memoryClockMin, memoryclock)
+        memoryclock = int(str(nvmlDeviceGetClockInfo(self.handle, 2)))
         return memoryclock
 
     def getGpuFanSpeed(self):
         """Get current speed of the fan"""
         gpufan = 0
         gpufan = int(str(nvmlDeviceGetFanSpeed(self.handle)))
-        self.maxfan = max(self.maxfan, gpufan)
-        self.minfan = min(self.minfan, gpufan)
         return gpufan
 
     def getGpuPowerUsage(self):
         """Get current power usage in Watts"""
         gpuPower = int(str(nvmlDeviceGetPowerUsage(self.handle)))
         gpuPower = int(round(gpuPower / 1000))
-        self.powerMax = max(self.powerMax, gpuPower)
-        self.powerMin = min(self.powerMin, gpuPower)
         return gpuPower
 
     def getGpuUtilization(self):
@@ -77,8 +76,6 @@ class NvidiaGPU():
         gpuUtil = gpuUtil.replace("c_nvmlUtilization_t(gpu: ", "")
         gpuUtil = gpuUtil.replace(" %", "")
         gpuUtil = int(gpuUtil)
-        self.gpuUtilMax = max(self.gpuUtilMax, gpuUtil)
-        self.gpuUtilMin = min(self.gpuUtilMin, gpuUtil)
         return gpuUtil
 
     def getMemoryUtilization(self):
@@ -86,8 +83,6 @@ class NvidiaGPU():
         memUtil = str(nvmlDeviceGetUtilizationRates(self.handle))
         memUtil = memUtil.split(",")[1].split(": ")[1].split(" ")[0]
         memUtil = int(memUtil)
-        self.memoryUtilMax = max(self.memoryUtilMax, memUtil)
-        self.memoryUtilMin = min(self.memoryUtilMin, memUtil)
         return memUtil
 
     def getDeviceName(self):
@@ -98,6 +93,41 @@ class NvidiaGPU():
     def getDriverVersion(self):
         version = str(nvmlSystemGetDriverVersion())
         return version
+
+    def getPerfCapReason(self):
+        reasons = {0: "None", 1: "GPU Idle", 2: "Application settings",
+                   4: "Software powercap", 8: "Hardware slowdown"
+                 }
+        print(nvmlDeviceGetCurrentClocksThrottleReasons(self.handle))
+        return reasons[nvmlDeviceGetCurrentClocksThrottleReasons(self.handle)]
+
+
+class ValueRecorder():
+    def __init__(self, func):
+        self.min = sys.maxsize
+        self.max = sys.maxsize * -1
+        self.sum = 0
+        self.count = 0
+        self.function = func
+        self.value = 0
+        self.average = 0
+
+    def update(self):
+        self.value = self.function()
+        self.min = min(self.value, self.min)
+        self.max = max(self.value, self.max)
+        try:
+            self.sum = self.sum + self.value
+            self.count = self.count + 1
+            self.average = int(round(self.sum/self.count, 0))
+        except OverflowError:
+            self.sum = int(floor(self.sum/1000))
+            self.count = int(floor(self.count/1000))
+
+
+
+
+
 
 
 if __name__ == '__main__':
